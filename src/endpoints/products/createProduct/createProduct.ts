7ba122c260b5cd6eec8e87227f42316c3f9ae5e1
Dacaramo <dankramirez@outlook.com>
2024-02-01 16:07:53 -0500

    refactor: createProduct COMPLETED

--- src/endpoints/products/createProduct/createProduct.ts
+++ src/endpoints/products/createProduct/createProduct.ts
@@ -7,8 +7,11 @@ import { inferRequestResponseFromError } from '../../../utils/inferRequestRespon
 import { Product, productSchema } from '../../../model/Product';
 import * as crypto from 'crypto';
 import { parse } from 'valibot';
-import { bucketNameSchema, indexNameSchema } from '../../../model/otherSchemas';
-import { createProductOnOpenSearchIndex } from './helpers';
+import { indexNameSchema, tableNameSchema } from '../../../model/otherSchemas';
+import {
+  createProductOnOpenSearchIndex,
+  validateSupportedLocaleIds,
+} from './helpers';
 import {
   validateProductCategoryIdAndSubcategoryId,
   validateProductColorId,
@@ -19,7 +22,7 @@ export const handler: APIGatewayProxyHandler = async (
 ): Promise<APIGatewayProxyResult> => {
   try {
     const indexName = process.env.OPENSEARCH_PRODUCTS_INDEX_NAME;
-    const bucketName = process.env.S3_BUCKET_NAME;
+    const tableName = process.env.DYNAMODB_SECONDARY_TABLE_NAME;
     const requestBody = JSON.parse(event.body!) as Omit<Product, 'pk'>;
     const document: Product = {
       pk: `product|${crypto.randomUUID()}`,
@@ -27,20 +30,25 @@ export const handler: APIGatewayProxyHandler = async (
     };
 
     const parsedIndexName = parse(indexNameSchema, indexName);
-    const parsedBucketName = parse(bucketNameSchema, bucketName);
-
+    const parsedTableName = parse(tableNameSchema, tableName);
     /* Additional validation, since it is already validated by API Gateway */
     const parsedDocument = parse(productSchema, document) as Product;
 
-    /* The categoryId and subcategoryId need to be validated against the available ids on the productCategories.json object on the S3 bucket */
+    /* The categoryId and subcategoryId needs to be validated against the available ids on dynamoDB */
     await validateProductCategoryIdAndSubcategoryId(
-      parsedBucketName,
-      document.categoryId,
-      document.subcategoryId
+      parsedTableName,
+      parsedDocument.categoryId,
+      parsedDocument.subcategoryId
     );
 
-    /* The colorId needs to be validated against the available ids on the productColors.json object on the S3 bucket */
-    await validateProductColorId(parsedBucketName, document.colorId);
+    /* The colorId needs to be validated against the available ids on dynamoDB */
+    await validateProductColorId(parsedTableName, parsedDocument.colorId);
+
+    /* All of the ids inside supportedLocaleIds needs to be validated against the ids on dynamoDB */
+    await validateSupportedLocaleIds(
+      parsedTableName,
+      parsedDocument.supportedLocaleIds
+    );
 
     await createProductOnOpenSearchIndex(parsedIndexName, parsedDocument);
     return {
--- src/endpoints/products/createProduct/helpers.ts
+++ src/endpoints/products/createProduct/helpers.ts
@@ -1,5 +1,7 @@
 import { Product } from '../../../model/Product';
 import { openSearchClient } from '../../../clients/openSearchClient';
+import { getSupportedLocales } from '../../helpers';
+import { ClientError } from '../../../model/Error';
 
 export const createProductOnOpenSearchIndex = async (
   indexName: string,
@@ -13,6 +15,24 @@ export const createProductOnOpenSearchIndex = async (
       refresh: true,
     });
     return apiResponse.meta;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const validateSupportedLocaleIds = async (
+  tableName: string,
+  idsToValidate: Array<string>
+): Promise<Array<string>> => {
+  try {
+    const supportedLocales = await getSupportedLocales(tableName);
+    const supportedLocaleIds = supportedLocales.map(({ id }) => id);
+    if (idsToValidate.every((id) => supportedLocaleIds.includes(id))) {
+      throw new ClientError(
+        'Some of the supportedLocaleIds are not supported. In order to be successfully create a product all of the localeIds inside the supportedLocaleIds of the product must be supported.'
+      );
+    }
+    return idsToValidate;
   } catch (error) {
     throw error;
   }
