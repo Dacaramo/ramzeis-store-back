9ba02b26ab87726b325939f1a8e4d42235cfd4fa
Dacaramo <dankramirez@outlook.com>
2024-02-01 09:24:55 -0500

    refactor: getProducts COMPLETED

--- src/endpoints/products/getProducts/getProducts.ts
+++ src/endpoints/products/getProducts/getProducts.ts
@@ -1,12 +1,102 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda';
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
+import { inferRequestResponseFromError } from '../../../utils/inferRequestResponseFromError';
+import {
+  Product,
+  ProductFilterValues,
+  ProductSort,
+  productFilterValuesSchema,
+} from '../../../model/Product';
+import { parse } from 'valibot';
+import { indexNameSchema, localeSchema } from '../../../model/otherSchemas';
+import {
+  getProductsFromOpenSearchIndex,
+  translateProductsNames,
+  translateToEnglish,
+} from './helpers';
+import { ListResponse } from '../../../model/ListResponse';
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
-) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      msg: 'This is a message',
-    }),
-  };
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const defaultLimit = 20;
+    const defaultSort = 'asc';
+    const defaultIsVisible = true;
+    const defaultIsForDropshipping = false;
+    const locale = event.pathParameters?.locale;
+    const qsp = event.queryStringParameters ?? {};
+    const productFilterValues: ProductFilterValues = {
+      limit: qsp.limit ? parseInt(qsp.limit) : defaultLimit,
+      encodedExclusiveStartKey: qsp.encodedExclusiveStartKey,
+      sort: qsp.sort ? (qsp.sort as ProductSort) : defaultSort,
+      search: qsp.search ? decodeURIComponent(qsp.search) : undefined,
+      categoryId: qsp.categoryId,
+      subcategoryId: qsp.subcategoryId,
+      colorId: qsp.colorId,
+      minPrice: qsp.minPrice ? parseInt(qsp.minPrice) : undefined,
+      maxPrice: qsp.maxPrice ? parseInt(qsp.maxPrice) : undefined,
+      isVisible: qsp.isVisible
+        ? qsp.isVisible === 'true'
+          ? true
+          : false
+        : defaultIsVisible,
+      isForDropshipping: qsp.isForDropshipping
+        ? qsp.isForDropshipping === 'true'
+          ? true
+          : false
+        : defaultIsForDropshipping,
+    };
+    const indexName = process.env.OPENSEARCH_PRODUCTS_INDEX_NAME;
+
+    const parsedLocale = parse(localeSchema, locale);
+    const parsedProductFilterValues = parse(
+      productFilterValuesSchema,
+      productFilterValues
+    );
+    const parsedIndexName = parse(indexNameSchema, indexName);
+
+    const languageCode = parsedLocale.split('-')[0];
+
+    /* If the search comes in a different language it must be translated to english before looking for the matching products on the index since everything on the index is stored in english */
+    if (
+      parsedProductFilterValues.search !== undefined &&
+      languageCode !== 'en'
+    ) {
+      parsedProductFilterValues.search = await translateToEnglish(
+        parsedProductFilterValues.search
+      );
+    }
+
+    const untranslatedData = await getProductsFromOpenSearchIndex(
+      parsedIndexName,
+      parsedProductFilterValues,
+      parsedLocale
+    );
+    let translatedData = undefined;
+
+    /* Translate only if the language code is not english, since all of the products are already in english in our database */
+    if (languageCode !== 'en') {
+      /* Translates only product names. The product details must be translated on the front-end since translating the details for all of the products is unnecessary and will increment Cloud Translation API costs*/
+      translatedData = await translateProductsNames(
+        untranslatedData.items,
+        languageCode
+      );
+    }
+
+    const readyData: ListResponse<Product> = {
+      ...untranslatedData,
+      items: translatedData ?? untranslatedData.items,
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(readyData),
+    };
+  } catch (error) {
+    return inferRequestResponseFromError(error);
+  }
 };
---
+++ src/endpoints/products/getProducts/helpers.ts
@@ -0,0 +1,142 @@
+import { ListResponse } from '../../../model/ListResponse';
+import { Product, ProductFilterValues } from '../../../model/Product';
+import { openSearchClient } from '../../../clients/openSearchClient';
+import { ExclusiveStartKey } from '../../../model/otherSchemas';
+import { SearchResponse } from '@opensearch-project/opensearch/api/types';
+import { googleTranslateClient } from '../../../clients/googleTranslateClient';
+import { decodeEsk } from '../../../utils/decodeEsk';
+
+export const getProductsFromOpenSearchIndex = async (
+  indexName: string,
+  productFilterValues: ProductFilterValues,
+  locale: string
+): Promise<ListResponse<Product>> => {
+  try {
+    const decodedEsk = productFilterValues.encodedExclusiveStartKey
+      ? decodeEsk(productFilterValues.encodedExclusiveStartKey)
+      : undefined;
+    const mustConditions = [
+      { term: { supportedLocales: locale } },
+      productFilterValues.search
+        ? {
+            match: {
+              name: {
+                query: productFilterValues.search,
+                operator: 'or',
+                fuzziness: 'AUTO',
+                case_insensitive: true,
+              },
+            },
+          }
+        : undefined,
+      productFilterValues.categoryId
+        ? { match: { categoryId: productFilterValues.categoryId } }
+        : undefined,
+      productFilterValues.subcategoryId
+        ? { match: { subcategoryId: productFilterValues.subcategoryId } }
+        : undefined,
+      productFilterValues.colorId
+        ? { match: { colorId: productFilterValues.colorId } }
+        : undefined,
+      productFilterValues.minPrice && !productFilterValues.maxPrice
+        ? { range: { price: { gte: productFilterValues.minPrice } } }
+        : undefined,
+      !productFilterValues.minPrice && productFilterValues.maxPrice
+        ? { range: { price: { lte: productFilterValues.maxPrice } } }
+        : undefined,
+      productFilterValues.minPrice && productFilterValues.maxPrice
+        ? {
+            range: {
+              price: {
+                gte: productFilterValues.minPrice,
+                lte: productFilterValues.maxPrice,
+              },
+            },
+          }
+        : undefined,
+      productFilterValues.isVisible !== undefined
+        ? { match: { isVisible: productFilterValues.isVisible } }
+        : undefined,
+      productFilterValues.isForDropshipping !== undefined
+        ? {
+            match: { isForDropshipping: productFilterValues.isForDropshipping },
+          }
+        : undefined,
+    ].filter((condition) => condition !== undefined);
+
+    const apiResponse = await openSearchClient.search({
+      index: indexName,
+      body: {
+        query: {
+          bool: {
+            must: mustConditions,
+          },
+        },
+        size: productFilterValues.limit,
+        sort: [{ price: productFilterValues.sort }, 'pk'],
+        ...(decodedEsk !== undefined
+          ? {
+              search_after: [decodedEsk.price, decodedEsk.pk],
+            }
+          : {}),
+      },
+    });
+    const searchResponse = apiResponse.body as SearchResponse<Product>;
+    const hits = searchResponse.hits.hits;
+    const items = hits.map((hit) => hit._source) as Array<Product>;
+    let lastEvaluatedKey: ExclusiveStartKey | undefined = undefined;
+    const count = hits.length;
+    const scannedCount = hits.length;
+
+    if (hits.length === productFilterValues.limit) {
+      const lastHit = hits[hits.length - 1];
+      if (lastHit.sort !== undefined && lastHit.sort.length === 2) {
+        lastEvaluatedKey = {
+          price: hits[hits.length - 1].sort![0],
+          pk: hits[hits.length - 1].sort![1],
+        };
+      }
+    }
+
+    return {
+      items,
+      lastEvaluatedKey,
+      count,
+      scannedCount,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const translateToEnglish = async (text: string) => {
+  try {
+    const [translatedText] = await googleTranslateClient.translate(text, 'en');
+    return translatedText;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const translateProductsNames = async (
+  products: Array<Product>,
+  languageCode: string
+): Promise<Array<Product>> => {
+  try {
+    const names = products.map(({ name }) => name);
+    const [translatedNames] = await googleTranslateClient.translate(
+      names,
+      languageCode
+    );
+    const translatedProducts = translatedNames.map((translatedName, i) => {
+      return {
+        ...products[i],
+        name: translatedName,
+      } as Product;
+    });
+
+    return translatedProducts;
+  } catch (error) {
+    throw error;
+  }
+};
